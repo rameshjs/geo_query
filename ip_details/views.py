a2d51c2c647d4a0b39a118717b51eddb98868886
Ramesh Kumar <rameshkumar.exe@gmail.com>
2023-11-10 04:19:52 +0530

    config: Drf configuration

--- ip_details/views.py
+++ ip_details/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import geoip2.database
 
-# Create your views here.
+
+def index(request):
+    with geoip2.database.Reader("GeoLite2-ASN.mmdb") as reader:
+        response = reader.asn("223.178.84.220")
+        print(response)
+    return
